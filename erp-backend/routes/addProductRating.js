5b774470f7f33104045ae88693ec4ce0ceaf43b9
Mukarram Ahmad <mukarram022@gmail.com>
2024-05-01 04:44:55 +0500

    rating added

---
+++ erp-backend/routes/addProductRating.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const { connection } = require("../database/ERP");
+
+router.post('/', (req, res) => {
+    const productid = req.body.productid;
+    const product = req.body.product;
+    const rating = req.body.rating;
+    const data = {
+        product_id: productid,
+        product_name: product,
+        rating: rating,
+    };
+    connection.query('INSERT INTO Product_Rating SET ?',data,(error,result)=>{
+        if(error)
+        {
+            console.log(error);
+        }else
+        {
+            console.log("Product Rating Added");
+            res.redirect('http://localhost:3000/ProductRating');
+        }
+    })
+});
+
+module.exports = router;
